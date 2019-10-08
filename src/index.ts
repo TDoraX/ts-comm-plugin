5ec8dab194bed29bd6ba892b0785bbff24efa302
TDoraX <zhangze612@163.com>
2019-10-08 20:27:58 +0800

    fix(url.ts & src/index.ts): Bug fixed

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,19 @@
 import { RequestOptions } from './types'
+import { buildURL } from './helpers/url'
 import xhr from './xhr'
 
 function AsyncRequest(config: RequestOptions): void {
+  processConfig(config)
   xhr(config)
+}
+
+function processConfig(config: RequestOptions): void {
+  config.url = transformUrl(config)
+}
+
+function transformUrl(config: RequestOptions): string {
+  const { url, params } = config
+  return buildURL(url, params)
 }
 
 export default AsyncRequest
